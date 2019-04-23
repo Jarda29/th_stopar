d4154286cfcbbff8322f4f32ddae800acf56f790
Jarda29 <langerjara@gmail.com>
2019-04-23 20:42:10 +0200

    Přidejte soubory projektu.

---
+++ th_stopar/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using th_stopar.Models;
+
+namespace th_stopar
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Game CurrentGame = new Game();
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpTheField();
+        }
+
+        private void SetUpTheField()
+        {
+            for (int i = 0; i < Game.Xsize; i++)
+            {
+                for (int j = 0; j < Game.Ysize; j++)
+                {
+                    Button btn = new Button
+                    {
+                        Content = "",
+                        Name = $"Button_{i}_{j}"
+                    };
+
+                    btn.Click += GameButton_Click;
+
+                    Grid.SetColumn(btn, j);
+                    Grid.SetRow(btn, i);
+                    gridGameTable.Children.Add(btn);
+                }
+            }
+        }
+
+        private void GameButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            var s = b.Name;
+
+            var posX = Convert.ToInt16(b.Name.Split('_')[1]);
+            var posY = Convert.ToInt16(b.Name.Split('_')[2]);
+
+            if(CurrentGame.Field[posX, posY] == Game.CellState.Throphy)
+            {
+                //Winner
+                MessageBox.Show("You are the winner");
+                return;
+            }
+
+            if(CurrentGame.Field[posX, posY] == Game.CellState.NearThrophy)
+            {
+                b.Content = "#";
+            }
+
+        }
+
+        private void BtnNewField_Click(object sender, RoutedEventArgs e)
+        {
+            SetUpTheField();
+        }
+    }
+}
---
+++ th_stopar/Models/Game.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace th_stopar.Models
+{
+    class Game
+    {
+        public const short Xsize = 4;
+        public const short Ysize = 4;
+
+        public CellState[,] Field { get; private set; }
+
+        public Game()
+        {
+            Field = new CellState[Xsize, Ysize]
+            {
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 }
+            };
+            PlaceAThrophy();
+        }
+
+        private void PlaceAThrophy()
+        {
+            var rnd = new Random();
+            var x = rnd.Next(4);
+            var y = rnd.Next(4);
+
+            Field[x, y] = CellState.Throphy;
+            for (int i = 0; i < Game.Xsize; i++)
+            {
+                if (i - 1 != x &&
+                        i != x &&
+                        i + 1 != x)
+                    continue;
+                for (int j = 0; j < Game.Ysize; j++)
+                {
+                    if (Field[i, j] == CellState.Throphy)
+                        continue;
+
+                    if (j - 1 != y &&
+                        j != y &&
+                        j + 1 != y)
+                        continue;
+
+                    Field[i, j] = CellState.NearThrophy;
+                }
+            }
+
+
+        }
+
+        public enum CellState
+        {
+            Empty = 0,
+            Throphy = 1,
+            NearThrophy = 2
+        }
+    }
+}
