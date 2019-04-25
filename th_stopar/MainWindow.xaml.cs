3fe6fbe7df78cf70c4b314141ed335eae4361b9a
Jaroslav Langer <langerjara@gmail.com>
2019-04-25 14:04:10 +0200

    Fixed some bugs. Added some stats.

--- th_stopar/MainWindow.xaml.cs
+++ th_stopar/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using th_stopar.Models;
@@ -10,31 +9,36 @@ namespace th_stopar
     /// <summary>
     /// Interakční logika pro MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
-        private Game CurrentGame;
-        private Player _playerOne;
-        private Player _playerTwo;
+        private Game _currentGame;
+        private readonly Player _playerOne;
+        private readonly Player _playerTwo;
         public List<Button> GameButtons = new List<Button>();
 
         private Player _playing;
-        private int _round = 0;
+        private bool _isSimulated;
+        private int _round;
+        private float _avgRounds;
+        private int _gamesPlayed;
         public MainWindow()
         {
             InitializeComponent();
+            _playerOne = new Player("Player 1", this);
+            _playerTwo = new Player("Player 2", this);
             SetUpTheField();
         }
 
         private void SetUpTheField()
         {
-            CurrentGame = new Game();
             GameButtons.Clear();
+            gridGameTable.Children.Clear();
             _round = 1;
-            for (int i = 0; i < Game.Xsize; i++)
+            for (var i = 0; i < Game.Xsize; i++)
             {
-                for (int j = 0; j < Game.Ysize; j++)
+                for (var j = 0; j < Game.Ysize; j++)
                 {
-                    Button btn = new Button
+                    var btn = new Button
                     {
                         Content = "",
                         Name = $"Button_{i}_{j}"
@@ -48,44 +52,42 @@ namespace th_stopar
                     GameButtons.Add(btn);
                 }
             }
-            _playerOne = new Player("Player 1", CurrentGame, this);
-            _playerTwo = new Player("Player 2", CurrentGame, this);
+            _currentGame = new Game();
             _playing = _playerOne;
         }
 
         private void GameButton_Click(object sender, RoutedEventArgs e)
         {
-            Button b = (Button)sender;
-            var s = b.Name;
+            var b = (Button)sender;
 
             var posX = Convert.ToInt32(b.Name.Split('_')[1]);
             var posY = Convert.ToInt32(b.Name.Split('_')[2]);
 
-            var test = CurrentGame.GetNearCells(posX, posY);
-
-            if(CurrentGame.Field[posX, posY] == Game.CellState.Throphy)
+            if (_currentGame.Field[posX, posY] == Game.CellState.Throphy)
             {
                 b.Content = Game.ThrophyMark;
-                MessageBox.Show($"{_playing.Name} is the winner (Round: {_round})");                
+                _gamesPlayed += 1;
+                _playing.Wins += 1;
+                _avgRounds = ((_avgRounds * (_gamesPlayed - 1)) + _round) / _gamesPlayed;
+                UpdateStatsLabels();
+                if (!_isSimulated)
+                    MessageBox.Show($"{_playing.Name} is the winner (Round: {_round})");
                 return;
             }
 
-            if(CurrentGame.Field[posX, posY] == Game.CellState.NearThrophy)
+            if (_currentGame.Field[posX, posY] == Game.CellState.NearThrophy)
             {
-                CurrentGame.FieldShared[posX, posY] = Game.CellState.NearThrophy;
+                _currentGame.FieldShared[posX, posY] = Game.CellState.NearThrophy;
                 b.Content = Game.NearByMark;
             }
             else
             {
-                CurrentGame.FieldShared[posX, posY] = Game.CellState.Empty;
+                _currentGame.FieldShared[posX, posY] = Game.CellState.Empty;
                 b.Content = Game.EmptyMark;
             }
             _round += 1;
-            if (_playing == _playerOne)
-                _playing = _playerTwo;
-            else
-                _playing = _playerOne;
-            _playing.Play();
+            _playing = _playing == _playerOne ? _playerTwo : _playerOne;
+            _playing.Play(_currentGame);
         }
 
         private void BtnNewField_Click(object sender, RoutedEventArgs e)
@@ -97,7 +99,41 @@ namespace th_stopar
         {
             _playerOne.PlayerStrategy = (Player.Strategy)strategyOne.SelectedIndex;
             _playerTwo.PlayerStrategy = (Player.Strategy)strategyTwo.SelectedIndex;
-            _playing.Play();
+            _playing.Play(_currentGame);
+        }
+
+        private void UpdateStatsLabels()
+        {
+            LblWinsP1.Content = $"{_playerOne.Wins} ({Math.Round((_playerOne.Wins / (float)_gamesPlayed) * 100, 1)} %)";
+            LblWinsP2.Content = $"{_playerTwo.Wins} ({Math.Round((_playerTwo.Wins / (float)_gamesPlayed) * 100, 1)} %)";
+            LblGames.Content = $"Games: {_gamesPlayed}";
+            LblAvgRounds.Content = $"Avg rounds: {Math.Round(_avgRounds, 1)}";
+        }
+
+        private void BtnSimulation100_OnClick(object sender, RoutedEventArgs e)
+        {
+            SetUpTheField();
+            _isSimulated = true;
+            var tempGamesPlayed = _gamesPlayed;
+            _playerOne.PlayerStrategy = (Player.Strategy)strategyOne.SelectedIndex;
+            _playerTwo.PlayerStrategy = (Player.Strategy)strategyTwo.SelectedIndex;
+            _playing.Play(_currentGame);
+            while (tempGamesPlayed + 99 >= _gamesPlayed)
+            {
+                SetUpTheField();
+                _playing.Play(_currentGame);
+            }
+            _isSimulated = false;
+        }
+
+        private void BtnClearStats_OnClick(object sender, RoutedEventArgs e)
+        {
+            _round = 0;
+            _gamesPlayed = 0;
+            _avgRounds = 0;
+            _playerOne.Wins = 0;
+            _playerTwo.Wins = 0;
+            UpdateStatsLabels();
         }
     }
 }
--- th_stopar/Models/Player.cs
+++ th_stopar/Models/Player.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 
 namespace th_stopar.Models
@@ -11,57 +10,59 @@ namespace th_stopar.Models
         public string Name { get; set; }
         public Strategy PlayerStrategy { get; set; }
 
-        private Game _game;
-        private MainWindow _window;
-        public Player(string name, Game game, MainWindow window)
+        public int Wins { get; set; }
+
+        private readonly MainWindow _window;
+
+        private readonly Random _random;
+        public Player(string name, MainWindow window)
         {
             Name = name;
-            _game = game;
             _window = window;
+            _random = new Random();
         }
-        public void Play()
+        public void Play(Game game)
         {
             if (PlayerStrategy == Strategy.Random)
                 PlayRandom();
             else if (PlayerStrategy == Strategy.Smart)
-                PlaySmart();
+                PlaySmart(game);
         }
         private void PlayRandom()
         {
-            var rnd = new Random();
-            var x = rnd.Next(Game.Xsize);
-            var y = rnd.Next(Game.Ysize);
+            var x = _random.Next(Game.Xsize);
+            var y = _random.Next(Game.Ysize);
 
 
-            var targetBtn = (Button)_window.GameButtons.First(bt => bt.Name == $"Button_{x}_{y}");
+            var targetBtn = _window.GameButtons.First(bt => bt.Name == $"Button_{x}_{y}");
             while (targetBtn.Content.ToString() != "")
             {
-                x = rnd.Next(Game.Xsize);
-                y = rnd.Next(Game.Ysize);
-                targetBtn = (Button)_window.GameButtons.First(bt => bt.Name == $"Button_{x}_{y}");
+                x = _random.Next(Game.Xsize);
+                y = _random.Next(Game.Ysize);
+                targetBtn = _window.GameButtons.First(bt => bt.Name == $"Button_{x}_{y}");
             }
             targetBtn.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
         }
-        private void PlaySmart()
+        private void PlaySmart(Game game)
         {
             var fieldP = new float[Game.Xsize, Game.Ysize];
             for (int k = 0; k < fieldP.GetLength(0); k++)
             {
                 for (int l = 0; l < fieldP.GetLength(1); l++)
                 {
-                    var nearCells = _game.GetNearCells(k, l);
-                    if (_game.FieldShared[k, l] == Game.CellState.NearThrophy)
+                    var nearCells = game.GetNearCells(k, l);
+                    if (game.FieldShared[k, l] == Game.CellState.NearThrophy)
                     {
                         var p = 1 / (float)nearCells.Count;
-                        foreach (var cells in _game.GetNearCells(k, l))
+                        foreach (var cells in game.GetNearCells(k, l))
                         {
                             fieldP[cells[0], cells[1]] += p;
                         }
                     }
-                    else if (_game.FieldShared[k, l] == Game.CellState.Empty)
+                    else if (game.FieldShared[k, l] == Game.CellState.Empty)
                     {
                         fieldP[k, l] = -1;
-                        foreach (var cells in _game.GetNearCells(k, l))
+                        foreach (var cells in game.GetNearCells(k, l))
                         {
                             fieldP[cells[0], cells[1]] = -1;
                         }
@@ -74,7 +75,7 @@ namespace th_stopar.Models
             }
 
 
-            var highestPCell = new int[2] { 0, 0 };
+            var highestPCell = new[] { 0, 0 };
             float maxP = 0;
             for (int k = 0; k < fieldP.GetLength(0); k++)
             {
@@ -88,7 +89,7 @@ namespace th_stopar.Models
                     maxP = fieldP[k, l];
                 }
             }
-            var targetBtn = (Button)_window.GameButtons.First(bt => bt.Name == $"Button_{highestPCell[0]}_{highestPCell[1]}");
+            var targetBtn = _window.GameButtons.First(bt => bt.Name == $"Button_{highestPCell[0]}_{highestPCell[1]}");
             targetBtn.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
         }
         public enum Strategy
